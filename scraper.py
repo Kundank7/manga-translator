d4816ebc8f4de96e955b556f3bc1e06efd50d6fe
Kundank7 <149052665+Kundank7@users.noreply.github.com>
2025-04-08 16:43:34 +0530

    scraper.py

---
+++ scraper.py
@@ -0,0 +1,7 @@
+import requests
+
+def download_image(url, save_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as f:
+            f.write(response.content)
